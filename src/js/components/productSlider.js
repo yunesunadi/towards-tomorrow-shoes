a5aa92eb7cb2789ef45e1a2ba5157be99f619b4a
Yune Su Nadi <yunesunadi.dev@gmail.com>
2022-09-09 22:01:21 +0630

    added popular now section

---
+++ src/js/components/productSlider.js
@@ -0,0 +1,53 @@
+import getElement from "../utilities/getElement";
+import "../render/slider";
+import "../render/product";
+
+const productContainer = getElement(".product-container");
+const prevBtn = getElement(".slider .prev-btn");
+const nextBtn = getElement(".slider .next-btn");
+
+const containerScrollWidth =
+    productContainer.scrollWidth - productContainer.clientWidth;
+
+const itemWidth = productContainer.children[0].getBoundingClientRect().width;
+
+const offsets = Array.from(productContainer.children)
+    .map((item, index) => itemWidth * index)
+    .slice(1);
+
+const disable = (btn) => {
+    btn.disabled = true;
+    btn.classList.add("disable");
+};
+
+const enable = (btn) => {
+    btn.disabled = false;
+    btn.classList.remove("disable");
+};
+
+disable(prevBtn);
+
+nextBtn.addEventListener("click", () => {
+    productContainer.scrollLeft += offsets[0];
+    enable(prevBtn);
+});
+
+productContainer.addEventListener("scroll", () => {
+    const scrollAmount = productContainer.scrollLeft;
+    const nearestOffset = offsets.find((offset) => offset > scrollAmount);
+
+    scrollAmount === 0 ? disable(prevBtn) : enable(prevBtn);
+    scrollAmount === containerScrollWidth ? disable(nextBtn) : enable(nextBtn);
+
+    nextBtn.addEventListener("click", () => {
+        productContainer.scrollLeft = nearestOffset;
+    });
+
+    prevBtn.addEventListener("click", () => {
+        if (offsets.includes(scrollAmount)) {
+            productContainer.scrollLeft = nearestOffset - offsets[0] * 2;
+        } else {
+            productContainer.scrollLeft = nearestOffset - offsets[0];
+        }
+    });
+});
