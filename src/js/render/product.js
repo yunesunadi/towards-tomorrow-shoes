f3e77e19ec6c1e4f55763c0b07c832904f5abf0b
Yune Su Nadi <yunesunadi.dev@gmail.com>
2022-09-20 21:08:06 +0630

    updated popular now section

--- src/js/render/product.js
+++ src/js/render/product.js
@@ -8,27 +8,27 @@ let elements = "";
 
 for (let i = 1; i <= quantity; i++) {
     elements += `
-    <a href="#" class="product__item-link">
-        <article class="product__item">
-            <figure class="product__img-container">
-                <img
-                    src="../../../dist/assets/images/placeholder-image.jpg"
-                    alt="Product image"
-                    class="product__img"
-                />
-                <i class="fa-regular fa-heart product__wishlist"></i>
-            </figure>
-            <div class="product__info">
-                <div class="product__info--left">
-                    <p class="product__name name">Name</p>
-                    <p class="product__category category">Category</p>
-                </div>
-                <div class="product__info--right">
-                    <p class="product__price price">Price</p>
-                </div>
+    <article class="product__item">
+        <figure class="product__img-container">
+            <img
+                src="../../../dist/assets/images/placeholder-image.jpg"
+                alt="Product placeholder image"
+                class="product__img"
+            />
+            <i
+                class="fa-regular fa-heart product__wishlist"
+            ></i>
+        </figure>
+        <div class="product__info">
+            <div class="product__info--left">
+                <p class="product__name name-placeholder">&nbsp;</p>
+                <p class="product__category category-placeholder">&nbsp;</p>
             </div>
-        </article>
-    </a>`;
+            <div class="product__info--right">
+                <p class="product__price price-placeholder">&nbsp;</p>
+            </div>
+        </div>
+    </article>`;
 }
 
 render(elements, ".product");
@@ -44,33 +44,39 @@ window.addEventListener("DOMContentLoaded", () => {
                 const suffix = gender === "Kids" ? "'" : "'s";
 
                 elements += `
-                <a href="product-details.html" class="product__item-link">
-                    <article class="product__item">
+                <article class="product__item">
+                    <a
+                        href="product-details.html"
+                        class="product__item-link"
+                    >
                         <figure class="product__img-container">
                             <img
                                 src=${imageUrls[0]}
                                 alt="Product image"
                                 class="product__img"
                             />
-                            <i class="fa-regular fa-heart product__wishlist"></i>
+                            <i
+                                class="fa-regular fa-heart product__wishlist"
+                            ></i>
                         </figure>
-                        <div class="product__info">
-                            <div class="product__info--left">
-                                <p class="product__name name">
-                                    ${name}
-                                </p>
-                                <p class="product__category category">
-                                    ${gender}${suffix} ${category} Shoes
-                                </p>
-                            </div>
-                            <div class="product__info--right">
-                                <p class="product__price price">
-                                    $${price}
-                                </p>
-                            </div>
+                    </a>
+                    <div class="product__info">
+                        <div class="product__info--left">
+                            <p class="product__name">
+                                <a
+                                    href="product-details.html"
+                                    class="product__item-link"
+                                >${name}</a>
+                            </p>
+                            <p class="product__category">
+                                ${gender}${suffix} ${category} Shoes
+                            </p>
                         </div>
-                    </article>
-                </a>`;
+                        <div class="product__info--right">
+                            <p class="product__price">$${price}</p>
+                        </div>
+                    </div>
+                </article>`;
 
                 render(elements, ".product");
             });
