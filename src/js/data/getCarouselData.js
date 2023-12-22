3ba0f06834dbd4c0c0c0e431cbca56b0880ebd45
Yune Su Nadi <yunesunadi.dev@gmail.com>
2023-12-22 18:09:03 +0630

    changed file path

--- src/js/data/getCarouselData.js
+++ src/js/data/getCarouselData.js
@@ -1,19 +1,19 @@
 const data = [
     {
         backgroundImage:
-            "src/assets/images/carousel-images/carousel-img1.jpg",
+            "dist/assets/images/carousel-images/carousel-img1.jpg",
         title: "Elevate Your Game",
         text: "Experience sporty fashion with Adidas shoes made for excellence.",
     },
     {
         backgroundImage:
-            "src/assets/images/carousel-images/carousel-img2.jpg",
+            "dist/assets/images/carousel-images/carousel-img2.jpg",
         title: "Unleash Your Potential",
         text: "High-performance shoes by Nike for achieving your best in style.",
     },
     {
         backgroundImage:
-            "src/assets/images/carousel-images/carousel-img3.jpg",
+            "dist/assets/images/carousel-images/carousel-img3.jpg",
         title: "Find Balance in Every Step",
         text: "Find support and comfort in New Balance shoes for your active life.",
     },
